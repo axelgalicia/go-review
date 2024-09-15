f85a347b0ec0bcd39e26cf050ca3b1c8ecbb4291
Axel Galicia <axelgalicia@gmail.com>
2024-09-15 13:17:20 -0400

    Learning about Go maps and slices

--- modules/greetings/greetings.go
+++ modules/greetings/greetings.go
@@ -24,3 +24,20 @@ func randomFormat() string {
 
 	return messages[rand.Intn(len(messages))]
 }
+
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages
+	messages := make(map[string]string)
+	// Lool through the recieved slices of names, calling
+	// the Hello function to get the message for each name
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+
+	return messages, nil
+}
--- modules/hello/hello.go
+++ modules/hello/hello.go
@@ -11,11 +11,14 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Axel")
+	names := []string{"Axel", "Anna", "Misato"}
+
+	// message, err := greetings.Hello("Axel")
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
